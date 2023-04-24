98bf2ec328c93b03df5ea3ce3d0f86ef7e0f7759
Gilberto Junior <gilberto@smartinnovation.com.br>
2023-04-24 15:40:32 -0300

    first commit

---
+++ exercise02/index.js
@@ -0,0 +1,43 @@
+const states = [
+  { acronym: "AC", name: "Acre" },
+  { acronym: "AL", name: "Alagoas" },
+  { acronym: "AP", name: "Amapá" },
+  { acronym: "AM", name: "Amazonas" },
+  { acronym: "BA", name: "Bahia" },
+  { acronym: "CE", name: "Ceará" },
+  { acronym: "DF", name: "Distrito Federal" },
+  { acronym: "ES", name: "Espírito Santo" },
+  { acronym: "GO", name: "Goiás" },
+  { acronym: "MA", name: "Maranhão" },
+  { acronym: "MT", name: "Mato Grosso" },
+  { acronym: "MS", name: "Mato Grosso do Sul" },
+  { acronym: "MG", name: "Minas Gerais" },
+  { acronym: "PA", name: "Pará" },
+  { acronym: "PB", name: "Paraíba" },
+  { acronym: "PR", name: "Paraná" },
+  { acronym: "PE", name: "Pernambuco" },
+  { acronym: "PI", name: "Piauí" },
+  { acronym: "RJ", name: "Rio de Janeiro" },
+  { acronym: "RN", name: "Rio Grande do Norte" },
+  { acronym: "RS", name: "Rio Grande do Sul" },
+  { acronym: "RO", name: "Rondônia" },
+  { acronym: "RR", name: "Roraima" },
+  { acronym: "SC", name: "Santa Catarina" },
+  { acronym: "SP", name: "São Paulo" },
+  { acronym: "SE", name: "Sergipe" },
+  { acronym: "TO", name: "Tocantins" },
+];
+
+// simulating fetch in an API
+function loadStates() {
+  const select = document.querySelector("#states");
+  states.forEach((state) => {
+    const option = document.createElement("option");
+    option.value = state.acronym;
+    option.text = `${state.name} - ${state.acronym}`;
+    select.appendChild(option);
+  });
+}
+
+
+window.addEventListener("load", loadStates);
